47e6e4cbfe5d737f0c6cb97080749077687d6094
DastanDevv <dastanalmaz6eko1ch@gmail.com>
2023-11-12 14:54:21 +0600

    news_models.dart

--- lib/constans/app_colors.dart
+++ lib/constans/app_colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class AppColos {
   static const Color orang = Color(0xffFE5722);
   static const Color black = Color(0xff000000);
+  static const Color white = Color(0xffffffff);
 }
