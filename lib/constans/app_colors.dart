567fcd47838597bf434263bb25ea83bcc4252d57
DastanDevv <dastanalmaz6eko1ch@gmail.com>
2023-11-15 12:55:33 +0600

    added new conts color

--- lib/constans/app_colors.dart
+++ lib/constans/app_colors.dart
@@ -4,4 +4,5 @@ class AppColors {
   static const Color orang = Color(0xffFE5722);
   static const Color balck = Color(0xff000000);
   static const Color white = Color(0xffFFFFFF);
+  static const Color grey = Color(0xff9E9E9E);
 }
